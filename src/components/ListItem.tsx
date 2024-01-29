99af3df121fad137bfef9fb855e998655bf353b0
Luis <luisfr364@gmail.com>
2024-01-28 21:17:33 -0300

    made some changes to the remove button and items overall

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -5,19 +5,30 @@ interface ListItemProp {
     id: number;
     title: string;
     addedDate: string;
-    delFunc: any;
+    delFunc: (id: number) => void;
 }
 
 function ListItem(props: ListItemProp) {
     return (
         <li key={props.key} className="list__item">
+            <label htmlFor="remove-button">
+                <span id="item__remove">X</span>
+            </label>
+            <button
+                className="hidden"
+                id="remove-button"
+                onClick={() => props.delFunc(props.id)}
+            >
+                Remove
+            </button>
             <div className="item__main-block">
                 <div className="item__block1">
                     <input type="checkbox" />
                     <h4>{props.title}</h4>
                 </div>
-                <p>{props.addedDate}</p>
-                <button onClick={() => props.delFunc(props.id)}>Remove</button>
+                <p className="item__date">
+                    Added: <br /> {props.addedDate}
+                </p>
             </div>
         </li>
     );
